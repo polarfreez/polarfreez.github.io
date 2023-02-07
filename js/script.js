d78e6527f720ab6704367f47b5b3b7caa250ea3e
Braunx <83966759+braunxx@users.noreply.github.com>
2023-02-07 10:53:11 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,3 +28,24 @@ setTimeout(type, 2000);
 }
 
 type();
+
+// Get the header element
+var header = document.getElementById("header");
+
+// Get the offset position of the header
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+  myFunction();
+};
+
