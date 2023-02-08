4fdb231ddf1db9b2704b6b1a0d837e7d4b4f4f15
Braunx <83966759+braunxx@users.noreply.github.com>
2023-02-08 08:55:36 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,9 +34,9 @@ type();
 window.onscroll = function() {
   var header = document.getElementsByClassName("header");
   if (window.pageYOffset > 50) {
-    header[0].style.backgroundColor = '#0d1017';
+    header[0].style.backgroundColor = 'rgba(0, 0, 0, 0)';
   } else {
-    header[0].style.backgroundColor = "#rgba(255, 0, 0, 1)";
+    header[0].style.backgroundColor = "#0d1017";
   }
 };
 
