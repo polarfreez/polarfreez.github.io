eb5210ef313fd412cd4ec20f4ffead7964e16bec
Braux <83966759+brauxx@users.noreply.github.com>
2023-07-18 15:17:09 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -2,26 +2,26 @@ let neon = document.querySelector('.neon');
 let isOn = true;
 
 setInterval(() => {
-if (isOn) {
-neon.classList.add('flicker');
-setTimeout(() => neon.classList.add('flicker-2'),500);
-setTimeout(() => neon.classList.add('flicker-3'),1000);
-setTimeout(() => {
-neon.classList.remove('flicker');
-neon.classList.remove('flicker-2');
-neon.classList.remove('flicker-3');
-neon.classList.add('off');
-neon.textContent = 'desenvolvedor';
-isOn = false;
-},1500);
-} else {
-neon.classList.remove('off');
-neon.textContent = 'artista';
-isOn = true;
-}
-},4000);
+  if (isOn) {
+    neon.classList.add('flicker');
+    setTimeout(() => neon.classList.add('flicker-2'), 500);
+    setTimeout(() => neon.classList.add('flicker-3'), 1000);
+    setTimeout(() => {
+      neon.classList.remove('flicker');
+      neon.classList.remove('flicker-2');
+      neon.classList.remove('flicker-3');
+      neon.classList.add('off');
+      neon.textContent = 'desenvolvedor';
+      isOn = false;
+    }, 1500);
+  } else {
+    neon.classList.remove('off');
+    neon.textContent = 'artista';
+    isOn = true;
+  }
+}, 4000);
 
-window.onscroll = function() {
+window.onscroll = function () {
   var header = document.getElementsByClassName("header");
   if (window.pageYOffset > 50) {
     header[0].style.backgroundColor = "#0d1017";
