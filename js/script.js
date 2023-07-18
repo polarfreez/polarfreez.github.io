645ffde036bcb5867b23322a55065adc1ad75ea6
Braux <83966759+brauxx@users.noreply.github.com>
2023-07-18 11:30:25 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-obfu_data = {
-    "delay":10,
-    "start_time":80,
-    "end_time":80,
-    "disp_time":2000,
-    "loop":true,
-    "obfu_chars":"123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@?#$%^&*()",
-    "phrases":[
-        'apaixonado pelo espaço e pelo universo',
-        'entusiasta da tecnologia',
-        'estudante',
-        'artista',
-        'desenvolvedor'
-  ]
-}
-
-
-
 window.onscroll = function() {
   var header = document.getElementsByClassName("header");
   if (window.pageYOffset > 50) {
