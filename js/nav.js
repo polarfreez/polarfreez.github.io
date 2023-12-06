f2f433217ea269a30fe01df42f7369deb8b3f691
AlphaZero <83966759+alphazero-dev@users.noreply.github.com>
2023-12-06 10:39:40 -0300

    Delete js/nav.js

--- js/nav.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById("toggle-sidebar");
-const sidebar = document.querySelector(".sidebar");
-
-toggleButton.addEventListener("click", function() {
-  sidebar.classList.toggle("active");
-});
