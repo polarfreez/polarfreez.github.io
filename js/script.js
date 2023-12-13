410ccacef0ec900d06be5eac8c107a00077b84f0
AlphaZero <83966759+alphazero-dev@users.noreply.github.com>
2023-12-13 13:31:30 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,78 +1,79 @@
 
-    $(function () {
-      $('.scroll-down').click(function () {
-        $('html, body').animate({ scrollTop: $('#about').offset().top + 80}, 'slow');
-        return false;
-      });
-    });
+$(function () {
+  $('.scroll-down').click(function () {
+    $('html, body').animate({ scrollTop: $('#about').offset().top + 80 }, 'slow');
+    return false;
+  });
+});
 
-    $(document).ready(function () {
-      $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        if (scroll > 10) {
-          $(".header").css("background", "rgb(13, 16, 23)");
-        }
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 10) {
+      $(".header").css("background", "rgb(13, 16, 23)");
+    }
 
-        else {
-          $(".header").css("background", "transparent");
-        }
-      })
-    })
+    else {
+      $(".header").css("background", "transparent");
+    }
+  })
+})
 
 $(function () {
 
   // overlay for smoother fullscreen enter
-var $overlay = $('<div class="fotorama-overlay"></div>')
-.css({ position: 'fixed', top: 0, right: 0, bottom: 0, left: 0, zIndex: 10001 })
-.animate()
-.fadeTo(0, 0)
-.hide()
-.appendTo('body');
-// take all .fotorama blocks
-$('.thumbs').each(function () {
-var $thumbs = $(this),
-  // clone it and make fotorama
-  $fotorama = $('.fotorama', $thumbs)
-    .clone()
-    //.show()
+  var $overlay = $('<div class="fotorama-overlay"></div>')
+    .css({ position: 'fixed', top: 0, right: 0, bottom: 0, left: 0, zIndex: 10001 })
     .animate()
-    .css({ position: 'fixed' })
-    .appendTo('body')
     .fadeTo(0, 0)
-    .fotorama(),
-  fotorama = $fotorama.data('fotorama');
+    .hide()
+    .appendTo('body');
+  // take all .fotorama blocks
+  $('.thumbs').each(function () {
+    var $thumbs = $(this),
+      // clone it and make fotorama
+      $fotorama = $('.fotorama', $thumbs)
+        .clone()
+        //.show()
+        .animate()
+        .css({ position: 'fixed' })
+        .appendTo('body')
+        .fadeTo(0, 0)
+        .fotorama(),
+      fotorama = $fotorama.data('fotorama');
 
-for (var _i = 0, _l = fotorama.data.length; _i < _l; _i++) {
-  // prepare id to use in fotorama.show()
-  fotorama.data[_i].id = fotorama.data[_i].img;
-}
+    for (var _i = 0, _l = fotorama.data.length; _i < _l; _i++) {
+      // prepare id to use in fotorama.show()
+      fotorama.data[_i].id = fotorama.data[_i].img;
+    }
 
-// bind clicks
-$thumbs.on('click', 'a', function (e) {
-  e.preventDefault();
+    // bind clicks
+    $thumbs.on('click', 'a', function (e) {
+      e.preventDefault();
 
-  var $this = $(this);
+      var $this = $(this);
 
-  $overlay
-    .show()
-    .stop()
-    .fadeTo(150, 1, function () {
-      $fotorama.stop().fadeTo(150, 1);
+      $overlay
+        .show()
+        .stop()
+        .fadeTo(150, 1, function () {
+          $fotorama.stop().fadeTo(150, 1);
 
-      // API calls
-      fotorama
-        // show needed frame
-        .show({ index: $this.attr('href'), time: 0 })
-        // open fullscreen
-        .requestFullScreen();
-    })
-    .animate();
-});
+          // API calls
+          fotorama
+            // show needed frame
+            .show({ index: $this.attr('href'), time: 0 })
+            // open fullscreen
+            .requestFullScreen();
+        })
+        .animate();
+    });
 
-$fotorama.on('fotorama:fullscreenexit', function () {
-  $fotorama.stop().fadeTo(0, 0);
-  $overlay.stop().fadeTo(300, 0, function () {
-    $overlay.hide();
+    $fotorama.on('fotorama:fullscreenexit', function () {
+      $fotorama.stop().fadeTo(0, 0);
+      $overlay.stop().fadeTo(300, 0, function () {
+        $overlay.hide();
+      });
+    });
   });
 });
-}); });
