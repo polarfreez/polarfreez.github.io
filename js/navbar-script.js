04308f7d1c68b9eb19e7260a634d7766b391f8c5
StoltZ <83966759+stoltz-dev@users.noreply.github.com>
2024-02-21 17:22:04 -0300

    Update navbar-script.js

--- js/navbar-script.js
+++ js/navbar-script.js
@@ -16,16 +16,18 @@ $(document).ready(function () {
     var menu = document.getElementById("navbar-menu");
 
     if (menu.classList.contains("active")){
-      menu.style.animation = "menu-out 0.5s ease forwards"
+      this.style.animation = "logo-rotate 0.5s ease forwards";
+      menu.style.animation = "menu-show 0.5s ease forwards";
       setTimeout(() => {
         menu.classList.remove("active");
         menu.style.display = 'none';
       }, 500);
 
     } else{
+      this.style.animation = "logoRotate 0.5s ease reverse";
       menu.style.display = 'flex';
       menu.classList.add("active");
-      menu.style.animation = "menu-in 0.5s ease forwards";
+      menu.style.animation = "menu-show 0.5s ease reverse";
     }
   });
 })
