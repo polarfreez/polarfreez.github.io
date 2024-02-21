994ea6a14b59d387b1fa2f78d7d509820ba7972b
StoltZ <83966759+stoltz-dev@users.noreply.github.com>
2024-02-21 19:29:38 -0300

    Update ai-background.js

--- js/ai-background.js
+++ js/ai-background.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function () {
   // Get the container element
   const background = document.getElementById("background");
   
