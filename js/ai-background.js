2a003acfb322e61039de376049223910d972ed29
StoltZ <83966759+stoltz-dev@users.noreply.github.com>
2024-02-26 02:56:45 -0300

    Update ai-background.js

--- js/ai-background.js
+++ js/ai-background.js
@@ -1,4 +1,4 @@
-document.onload = function () {  
+$(document).ready(function () {  
   // Get the container element
   const background = document.getElementById("background");
   
@@ -121,4 +121,4 @@ document.onload = function () {
   }); 
    }
   }, 250);
-};
+});
